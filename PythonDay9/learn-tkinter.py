ae796dc3469d089d0fb8257246859e21e1af9066
mandashaumy <amandyshaumy@gmail.com>
2026-09-01 08:47:21 +0700

    add day 9

---
+++ PythonDay9/learn-tkinter.py
@@ -0,0 +1,9 @@
+import customtkinter as ctk
+app = ctk.CTk()
+app.title("Learn Tkinter First")
+app.geometry("400x600")
+app.resizable(False, False)
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("blue")
+
+app.mainloop()
